cb5205fb63a896f0419c95b978b2dee568d5add3
Alexey Smychagin <smalex69@gmail.com>
2017-07-19 21:43:16 -0400

    add keys for tests

---
+++ server/common/keys/create.sh
@@ -0,0 +1,9 @@
+
+openssl genrsa -out app.rsa 1024
+openssl rsa -in app.rsa -pubout -outform PEM -out app.rsa.pub
+
+# ssh-keygen -t rsa -b 4096 -f jwtRS256.key
+# # Don't add passphrase
+# openssl rsa -in jwtRS256.key -pubout -outform PEM -out jwtRS256.key.pub
+# cat jwtRS256.key
+# cat jwtRS256.key.pub
